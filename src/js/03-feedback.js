cb585d40ef42c8b335bdfe7df1c21655b346ddbc
Ostap <Ostapkriven13@gmail.com>
2023-06-30 10:48:44 +0300

    і
    і

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+
+const form = document.getElementById('feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+form.addEventListener('input', throttle(saveFormState, 500));
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+window.addEventListener('load', () => {
+    const savedFormState = localStorage.getItem('feedback-form-state');
+    if (savedFormState) {
+      const formState = JSON.parse(savedFormState);
+      emailInput.value = formState.email;
+      messageInput.value = formState.message;
+    }
+  });
+
+  form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+
+  emailInput.value = '';
+  messageInput.value = '';
+
+
+  console.log(formState);
+
+
+  localStorage.removeItem('feedback-form-state');
+}
 
