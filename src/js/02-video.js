0dc309df67c705ebf2dd2006a31fc57f9b8a077c
Ostap <Ostapkriven13@gmail.com>
2023-06-30 10:46:18 +0300

    і
    і

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }
+
+  window.addEventListener('load', () => {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+      player.setCurrentTime(currentTime);
+    }
+  });
+  
