7141fea1e5142cfdd9363b4cc9ef32281cdb0f53
Ostap <Ostapkriven13@gmail.com>
2023-06-30 10:31:12 +0300

    s
    s

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,8 +9,8 @@ const gallery = document.querySelector('.gallery');
 
 
 const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
-  <li>
-    <a class="gallery__item" href="${original}">
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
   </li>
