40c3eb444a6184b5635537095433393292883456
Ostap <Ostapkriven13@gmail.com>
2023-07-06 13:26:44 +0300

    і
    і

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -17,4 +17,16 @@ function saveCurrentTime(event) {
       player.setCurrentTime(currentTime);
     }
   });
-  
+  const express = require('express');
+const throttle = require('express-throttle');
+
+const app = express();
+
+// Обмеження до 100 запитів на хвилину
+app.use(throttle({ rate: '100/minute' }));
+
+// Додайте решту вашого коду маршрутизації
+
+app.listen(3000, () => {
+  console.log('Сервер запущено на порту 3000');
+});
